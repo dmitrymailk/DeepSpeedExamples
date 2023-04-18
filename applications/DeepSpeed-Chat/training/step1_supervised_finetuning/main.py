dd8d5745779c0e83c004a36d8b437cfce7463ca7
dmitrymailk <dmitrymail_k@mail.ru>
2023-04-18 03:18:14 +0300

    fix eval and train scripts

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/main.py
@@ -24,48 +24,64 @@ import deepspeed
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 
 sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
 from utils.data.data_utils import create_prompt_dataset
-from utils.utils import print_rank_0, to_device, save_hf_format, set_random_seed, get_all_reduce_mean, get_optimizer_grouped_parameters, save_zero_three_model
+from utils.utils import (
+    print_rank_0,
+    to_device,
+    save_hf_format,
+    set_random_seed,
+    get_all_reduce_mean,
+    get_optimizer_grouped_parameters,
+    save_zero_three_model,
+)
 from utils.ds_utils import get_train_ds_config
-from utils.module.lora import convert_linear_layer_to_lora, convert_lora_to_linear_layer, only_optimize_lora_parameters
+from utils.module.lora import (
+    convert_linear_layer_to_lora,
+    convert_lora_to_linear_layer,
+    only_optimize_lora_parameters,
+)
 from utils.model.model_utils import create_hf_model
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description=
-        "Finetune a transformers model on a causal language modeling task")
-    parser.add_argument('--data_path',
-                        nargs='*',
-                        default=['Dahoas/rm-static'],
-                        help='Path to the training dataset. Accepted format:'
-                        '1) a single data path, 2) multiple datasets in the'
-                        'form: dataset1-path dataset2-path ...')
-    parser.add_argument('--data_split',
-                        type=str,
-                        default='6,2,2',
-                        help='Comma-separated list of proportions for training'
-                        'phase 1, 2, and 3 data. For example the split `2,4,4`'
-                        'will use 60% of data for phase 1, 20% for phase 2'
-                        'and 20% for phase 3.')
+        description="Finetune a transformers model on a causal language modeling task"
+    )
     parser.add_argument(
-        '--sft_only_data_path',
-        nargs='*',
-        default=[],
-        help='Path to the dataset for only using in SFT phase.')
+        "--data_path",
+        nargs="*",
+        default=["Dahoas/rm-static"],
+        help="Path to the training dataset. Accepted format:"
+        "1) a single data path, 2) multiple datasets in the"
+        "form: dataset1-path dataset2-path ...",
+    )
     parser.add_argument(
-        '--data_output_path',
+        "--data_split",
         type=str,
-        default='/tmp/data_files/',
-        help=
-        'Where to store the data-related files such as shuffle index. This needs to be on a local storage of a node (not on a shared storage)'
+        default="6,2,2",
+        help="Comma-separated list of proportions for training"
+        "phase 1, 2, and 3 data. For example the split `2,4,4`"
+        "will use 60% of data for phase 1, 20% for phase 2"
+        "and 20% for phase 3.",
+    )
+    parser.add_argument(
+        "--sft_only_data_path",
+        nargs="*",
+        default=[],
+        help="Path to the dataset for only using in SFT phase.",
+    )
+    parser.add_argument(
+        "--data_output_path",
+        type=str,
+        default="/tmp/data_files/",
+        help="Where to store the data-related files such as shuffle index. This needs to be on a local storage of a node (not on a shared storage)",
     )
     parser.add_argument(
         "--model_name_or_path",
         type=str,
-        help=
-        "Path to pretrained model or model identifier from huggingface.co/models.",
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
         required=True,
     )
     parser.add_argument(
@@ -90,23 +106,22 @@ def parse_args():
         "--learning_rate",
         type=float,
         default=1e-3,
-        help=
-        "Initial learning rate (after the potential warmup period) to use.",
+        help="Initial learning rate (after the potential warmup period) to use.",
     )
-    parser.add_argument("--weight_decay",
-                        type=float,
-                        default=0.1,
-                        help="Weight decay to use.")
-    parser.add_argument("--num_train_epochs",
-                        type=int,
-                        default=1,
-                        help="Total number of training epochs to perform.")
+    parser.add_argument(
+        "--weight_decay", type=float, default=0.1, help="Weight decay to use."
+    )
+    parser.add_argument(
+        "--num_train_epochs",
+        type=int,
+        default=1,
+        help="Total number of training epochs to perform.",
+    )
     parser.add_argument(
         "--gradient_accumulation_steps",
         type=int,
         default=1,
-        help=
-        "Number of updates steps to accumulate before performing a backward/update pass.",
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
     )
     parser.add_argument(
         "--lr_scheduler_type",
@@ -114,51 +129,65 @@ def parse_args():
         default="cosine",
         help="The scheduler type to use.",
         choices=[
-            "linear", "cosine", "cosine_with_restarts", "polynomial",
-            "constant", "constant_with_warmup"
+            "linear",
+            "cosine",
+            "cosine_with_restarts",
+            "polynomial",
+            "constant",
+            "constant_with_warmup",
         ],
     )
     parser.add_argument(
         "--num_warmup_steps",
         type=int,
         default=0,
-        help="Number of steps for the warmup in the lr scheduler.")
-    parser.add_argument("--output_dir",
-                        type=str,
-                        default=None,
-                        help="Where to store the model.")
-    parser.add_argument("--seed",
-                        type=int,
-                        default=1234,
-                        help="A seed for reproducible training.")
-    parser.add_argument("--local_rank",
-                        type=int,
-                        default=-1,
-                        help="local_rank for distributed training on gpus")
-    parser.add_argument('--gradient_checkpointing',
-                        action='store_true',
-                        help='Enable HF gradient checkpointing for model.')
-    # deepspeed features
-    parser.add_argument('--offload',
-                        action='store_true',
-                        help='Enable ZeRO Offload techniques.')
+        help="Number of steps for the warmup in the lr scheduler.",
+    )
     parser.add_argument(
-        '--zero_stage',
+        "--output_dir", type=str, default=None, help="Where to store the model."
+    )
+    parser.add_argument(
+        "--seed", type=int, default=1234, help="A seed for reproducible training."
+    )
+    parser.add_argument(
+        "--local_rank",
+        type=int,
+        default=-1,
+        help="local_rank for distributed training on gpus",
+    )
+    parser.add_argument(
+        "--gradient_checkpointing",
+        action="store_true",
+        help="Enable HF gradient checkpointing for model.",
+    )
+    # deepspeed features
+    parser.add_argument(
+        "--offload", action="store_true", help="Enable ZeRO Offload techniques."
+    )
+    parser.add_argument(
+        "--zero_stage",
         type=int,
         default=0,
-        help='ZeRO optimization stage for Actor model (and clones).')
+        help="ZeRO optimization stage for Actor model (and clones).",
+    )
     ## LoRA for efficient training setting
-    parser.add_argument("--lora_dim",
-                        type=int,
-                        default=0,
-                        help="If > 0, use LoRA for efficient training.")
-    parser.add_argument("--lora_module_name",
-                        type=str,
-                        default="decoder.layers.",
-                        help="The scope of LoRA.")
-    parser.add_argument('--only_optimize_lora',
-                        action='store_true',
-                        help='Only optimize the LoRA parameters.')
+    parser.add_argument(
+        "--lora_dim",
+        type=int,
+        default=0,
+        help="If > 0, use LoRA for efficient training.",
+    )
+    parser.add_argument(
+        "--lora_module_name",
+        type=str,
+        default="decoder.layers.",
+        help="The scope of LoRA.",
+    )
+    parser.add_argument(
+        "--only_optimize_lora",
+        action="store_true",
+        help="Only optimize the LoRA parameters.",
+    )
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
 
@@ -177,6 +206,8 @@ def main():
     if args.local_rank == -1:
         device = torch.device("cuda")
     else:
+        # устанавливаем текущий девайс, наверное это сделано глобально
+        # альтернатива torch.cuda.device(device)
         torch.cuda.set_device(args.local_rank)
         device = torch.device("cuda", args.local_rank)
         # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
@@ -185,31 +216,41 @@ def main():
 
     args.global_rank = torch.distributed.get_rank()
 
-    ds_config = get_train_ds_config(offload=args.offload,
-                                    stage=args.zero_stage)
-    ds_config[
-        'train_micro_batch_size_per_gpu'] = args.per_device_train_batch_size
-    ds_config[
-        'train_batch_size'] = args.per_device_train_batch_size * torch.distributed.get_world_size(
-        ) * args.gradient_accumulation_steps
+    # возвращает обычный объект с параметрами
+    ds_config = get_train_ds_config(offload=args.offload, stage=args.zero_stage)
+    ds_config["train_micro_batch_size_per_gpu"] = args.per_device_train_batch_size
+    ds_config["train_batch_size"] = (
+        args.per_device_train_batch_size
+        * torch.distributed.get_world_size()
+        * args.gradient_accumulation_steps
+    )
 
     # If passed along, set the training seed now.
     set_random_seed(args.seed)
 
     assert not args.offload, "zero-offload is not currently supported but coming soon!"
-
+    # https://pytorch.org/docs/stable/distributed.html#torch.distributed.barrier
+    # синхронизирует все процессы
     torch.distributed.barrier()
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path,
-                                              fast_tokenizer=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.model_name_or_path,
+        fast_tokenizer=True,
+    )
     tokenizer.pad_token = tokenizer.eos_token
 
-    model = create_hf_model(AutoModelForCausalLM, args.model_name_or_path,
-                            tokenizer, ds_config)
+    # просто импортим модель с hf, только зачем-то делаем вокабуляр кратным 8
+    model = create_hf_model(
+        AutoModelForCausalLM,
+        args.model_name_or_path,
+        tokenizer,
+        ds_config,
+    )
 
     if args.lora_dim > 0:
-        model = convert_linear_layer_to_lora(model, args.lora_module_name,
-                                             args.lora_dim)
+        model = convert_linear_layer_to_lora(
+            model, args.lora_module_name, args.lora_dim
+        )
         if args.only_optimize_lora:
             model = only_optimize_lora_parameters(model)
 
@@ -224,7 +265,8 @@ def main():
         args.seed,
         tokenizer,
         args.max_seq_len,
-        sft_only_data_path=args.sft_only_data_path)
+        sft_only_data_path=args.sft_only_data_path,
+    )
 
     # DataLoaders creation:
     if args.local_rank == -1:
@@ -233,14 +275,18 @@ def main():
     else:
         train_sampler = DistributedSampler(train_dataset)
         eval_sampler = DistributedSampler(eval_dataset)
-    train_dataloader = DataLoader(train_dataset,
-                                  collate_fn=default_data_collator,
-                                  sampler=train_sampler,
-                                  batch_size=args.per_device_train_batch_size)
-    eval_dataloader = DataLoader(eval_dataset,
-                                 collate_fn=default_data_collator,
-                                 sampler=eval_sampler,
-                                 batch_size=args.per_device_eval_batch_size)
+    train_dataloader = DataLoader(
+        train_dataset,
+        collate_fn=default_data_collator,
+        sampler=train_sampler,
+        batch_size=args.per_device_train_batch_size,
+    )
+    eval_dataloader = DataLoader(
+        eval_dataset,
+        collate_fn=default_data_collator,
+        sampler=eval_sampler,
+        batch_size=args.per_device_eval_batch_size,
+    )
 
     def evaluation(model, eval_dataloader):
         model.eval()
@@ -265,15 +311,17 @@ def main():
 
     # Split weights in two groups, one with weight decay and the other not.
     optimizer_grouped_parameters = get_optimizer_grouped_parameters(
-        model, args.weight_decay)
+        model, args.weight_decay
+    )
 
     AdamOptimizer = DeepSpeedCPUAdam if args.offload else FusedAdam
-    optimizer = AdamOptimizer(optimizer_grouped_parameters,
-                              lr=args.learning_rate,
-                              betas=(0.9, 0.95))
+    optimizer = AdamOptimizer(
+        optimizer_grouped_parameters, lr=args.learning_rate, betas=(0.9, 0.95)
+    )
 
     num_update_steps_per_epoch = math.ceil(
-        len(train_dataloader) / args.gradient_accumulation_steps)
+        len(train_dataloader) / args.gradient_accumulation_steps
+    )
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optimizer,
@@ -287,7 +335,8 @@ def main():
         args=args,
         config=ds_config,
         lr_scheduler=lr_scheduler,
-        dist_init_required=True)
+        dist_init_required=True,
+    )
 
     if args.gradient_checkpointing:
         model.gradient_checkpointing_enable()
@@ -296,14 +345,16 @@ def main():
     print_rank_0("***** Running training *****", args.global_rank)
     print_rank_0(
         f"***** Evaluating perplexity, Epoch {0}/{args.num_train_epochs} *****",
-        args.global_rank)
+        args.global_rank,
+    )
     perplexity = evaluation(model, eval_dataloader)
     print_rank_0(f"ppl: {perplexity}", args.global_rank)
 
     for epoch in range(args.num_train_epochs):
         print_rank_0(
             f"Beginning of Epoch {epoch+1}/{args.num_train_epochs}, Total Micro Batches {len(train_dataloader)}",
-            args.global_rank)
+            args.global_rank,
+        )
         model.train()
         for step, batch in enumerate(train_dataloader):
             batch = to_device(batch, device)
@@ -315,13 +366,14 @@ def main():
         # Evaluate perplexity on the validation set.
         print_rank_0(
             f"***** Evaluating perplexity, Epoch {epoch+1}/{args.num_train_epochs} *****",
-            args.global_rank)
+            args.global_rank,
+        )
         perplexity = evaluation(model, eval_dataloader)
         print_rank_0(f"ppl: {perplexity}", args.global_rank)
         model.tput_timer.update_epoch_count()
 
     if args.output_dir is not None:
-        print_rank_0('saving the final model ...', args.global_rank)
+        print_rank_0("saving the final model ...", args.global_rank)
         model = convert_lora_to_linear_layer(model)
 
         if args.global_rank == 0:
@@ -329,10 +381,9 @@ def main():
 
         if args.zero_stage == 3:
             # For zero stage 3, each gpu only has a part of the model, so we need a special save function
-            save_zero_three_model(model,
-                                  args.global_rank,
-                                  args.output_dir,
-                                  zero_stage=args.zero_stage)
+            save_zero_three_model(
+                model, args.global_rank, args.output_dir, zero_stage=args.zero_stage
+            )
 
 
 if __name__ == "__main__":
--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation.
 # SPDX-License-Identifier: Apache-2.0
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 # DeepSpeed Team
 OUTPUT=$1
 ZERO_STAGE=$2
@@ -13,21 +13,20 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 
-deepspeed main.py \
+nohup deepspeed main.py \
    --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets openai/webgpt_comparisons stanfordnlp/SHP \
    --data_split 2,4,4 \
    --model_name_or_path facebook/opt-1.3b \
-   --per_device_train_batch_size 8 \
-   --per_device_eval_batch_size 8 \
+   --per_device_train_batch_size 6 \
+   --per_device_eval_batch_size 6 \
    --max_seq_len 512 \
    --learning_rate 9.65e-6 \
    --weight_decay 0.1 \
    --num_train_epochs 2 \
-   --gradient_accumulation_steps 1 \
+   --gradient_accumulation_steps 8 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
-   --output_dir $OUTPUT \
-   &> $OUTPUT/training.log
+   --output_dir $OUTPUT > $OUTPUT/training.log &
